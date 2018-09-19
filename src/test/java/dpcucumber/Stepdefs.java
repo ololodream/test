243e154b0b4c25f79a9f8a3b1afb856b5088214f
mengjia <cmengj02@gmail.com>
2018-09-19 14:14:17 +1000

    Update Stepdefs.java

--- src/test/java/dpcucumber/Stepdefs.java
+++ src/test/java/dpcucumber/Stepdefs.java
@@ -1,118 +1,56 @@
-package dpcucumber;
+package com.example.bookingdemo;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.When;
-import files.Payload;
-import files.ReusableMethods;
-import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import cucumber.api.java.en.Then;
+import org.springframework.web.bind.annotation.*;
 
-import static io.restassured.RestAssured.given;
-import static org.junit.Assert.*;
-
-import java.io.FileInputStream;
 import java.util.ArrayList;
-import java.util.Properties;
+import java.util.List;
+import java.util.stream.Collectors;
 
-
-public class Stepdefs {
-    Properties prop = new Properties();
-
-    private Response create_res;
-    private Response all_res;
-    private Response delete_res;
-    private  String id;
-
-
-    @Given("^a user want to get all bookings$")
-    public void a_user_want_to_get_all_bookings() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        FileInputStream files = new FileInputStream("src\\test\\java\\config\\env.properties");
-        prop.load(files);
-    }
-    @When("^a request is sent$")
-    public void a_request_is_sent() throws Exception {
-        RestAssured.baseURI= prop.getProperty("BookingsHost");
-        this.all_res = given().
-                when().get("/bookings/all");
-    }
-    @Then("^all bookings should be returned$")
-    public void all_bookings_should_be_returned() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        assertEquals(200,all_res.statusCode());
+@RestController
+@RequestMapping(value = "/bookings")
+public class BookingController {
+    /*private List<HotelBooking> bookings;*/
+    BookingRepository bookingRepository;
+    public BookingController(BookingRepository bookingRepository)
+    {
+        this.bookingRepository = bookingRepository;
     }
 
-    @Given("^a booking record$")
-    public void a_booking_record() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        FileInputStream files = new FileInputStream("src\\test\\java\\config\\env.properties");
-        prop.load(files);
+   /* public BookingController(){
+        bookings = new ArrayList<>();
+        bookings.add(new HotelBooking("Marriot", 200.50,3));
+        bookings.add(new HotelBooking("Ibis", 90,4));
+        bookings.add(new HotelBooking("Novotel", 140,1));
+    }*/
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<HotelBooking> getAll()
+    {
+        /*return bookings;*/
+        return this.bookingRepository.findAll();
     }
 
-    @When("^request to create a new booking$")
-    public void request_to_create_a_new_booking() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        RestAssured.baseURI= prop.getProperty("BookingsHost");
-        this.create_res = given().header("Content-Type","application/json").
-                body(Payload.getCreateBooking()).
-                when().post("/bookings/create");
+    /*@RequestMapping(value = "/affordable/{price}", method = RequestMethod.GET)
+    public List<HotelBooking> getAffordable(@PathVariable double price)
+    {
+        *//*return bookings.stream().filter(x -> x.getPricePerNight() < price ).collect(Collectors.toList());*//*
+        return this.bookingRepository.findByPricePerNightLessThan(price);
+    }*/
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public List<HotelBooking> create(@RequestBody HotelBooking hotelBooking)
+    {
+        /*bookings.add(hotelBooking);
+        return bookings;*/
+        bookingRepository.save(hotelBooking);
+        return bookingRepository.findAll();
     }
 
-    @Then("^a new booking should be created$")
-    public void a_new_booking_should_be_created() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        String res = this.create_res.asString();
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public List<HotelBooking> delete(@PathVariable long id)
+    {
 
-        JsonPath js = new JsonPath(res);
-        int count = js.getInt("size");
-        System.out.println("******************************");
-//        System.out.println(res);
-        //get last id
-        /*JsonPath.read(jsonString, sourceFieldFormula);*/
-//       this.id = com.jayway.jsonpath.JsonPath.read(res,"$.[(@.length-1)].id");
-        ArrayList<String> ids = js.get("id");
-        System.out.println(ids);
-        System.out.println(ids.size());
-        ids.get(ids.size()-1);
-
-        System.out.println(String.valueOf(ids.get(ids.size()-1)));
-//        System.out.println(ids.get(ids.size()-1).toString());
-        System.out.println("=====after  ids.get(ids.size()-1);=========");
-
-        this.id = String.valueOf(ids.get(ids.size()-1));
-
-        System.out.println(this.id);
-
-        assertEquals(200,create_res.statusCode());
+        bookingRepository.deleteById(id);
+        return bookingRepository.findAll();
     }
-
-    @Given("^a booing id$")
-    public void a_booing_id() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-
-        FileInputStream files = new FileInputStream("src\\test\\java\\config\\env.properties");
-        prop.load(files);
-
-    }
-
-    @When("^request to delete this record$")
-    public void request_to_delete_this_record() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        String id = "3";
-        RestAssured.baseURI= prop.getProperty("BookingsHost");
-        this.delete_res = given().pathParam("id",id).
-                when().get("/bookings/delete/{id}");
-    }
-
-    @Then("^this booking record should be deleted$")
-    public void this_booking_record_should_be_deleted() throws Exception {
-        // Write code here that turns the phrase above into concrete actions
-        assertEquals(200,delete_res.statusCode());
-    }
-
-
-
-
 }
